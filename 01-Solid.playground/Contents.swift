55ed1f39f6e7d6bee5d93ac5e562226327d9a18a
Ratul Chhibber <ratulchhibber@gmail.com>
2021-11-16 16:47:30 +0530

    * SOLID Principles

---
+++ 01-Solid.playground/Contents.swift
@@ -0,0 +1,18 @@
+/**
+ 1. **Single Responsiblity Principle**-
+ 1.a. A class should have only one reason to change.
+ 1.b. Separation of concerns - different classes handling different, independent tasks/ problems.
+ 
+ 2. **Open-Closed Principle**-
+ 2.a. Classes should be open for extension but closed for modification
+ 
+ 3. **Liskov Substitution Principle**-
+ 3.a. You should be able to substitute a base class with a subclass with no side-effects.
+ 
+ 4. **Interface Segregation Principle**-
+ 4.a. Don't put too muich into an interface, split into intefaces.
+ 4.b. YAGNI- You Ain't Going to Need it.
+ 
+ 5. **Dependency INversion Principle**-
+ 5.a. High-level modules should not depend on low-level modules- They should depend on abstractions.
+ */
