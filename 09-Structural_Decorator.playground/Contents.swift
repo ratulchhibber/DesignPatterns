b3ac313a1d1778c04813bea80ebadf686ae4429d
Ratul Chhibber <ratulchhibber@gmail.com>
2021-11-19 16:16:50 +0530

    * Decorator - Exercise

--- 09-Structural_Decorator.playground/Contents.swift
+++ 09-Structural_Decorator.playground/Contents.swift
@@ -56,3 +56,50 @@ func main()
 //Read Specification pattern- https://medium.com/swift2go/mastering-generics-with-protocols-the-specification-pattern-5e2e303af4ca
 
 main()
+
+/*
+ Exercise-
+  The following code scenario shows a 'Dragon' which is both a 'Bird' and a 'Lizard'.
+  Complete the Dragon's interface (there's no need to extract protocols out of either
+  'Bird' or 'Lizard'). Take special care when implementing the 'age' property!
+ */
+
+class Bird {
+    var age = 0
+
+    func fly() -> String {
+        return (age < 10) ? "flying" : "too old"
+    }
+}
+
+class Lizard {
+    var age = 0
+
+    func crawl() -> String {
+        return (age > 1) ? "crawling" : "too young"
+    }
+}
+
+// Solution
+class Dragon {
+    private let bird = Bird()
+    private let lizard = Lizard()
+
+    var age: Int {
+        set {
+            bird.age = newValue
+            lizard.age = newValue
+        }
+        get {
+            return age
+        }
+    }
+
+    func fly() -> String {
+        return bird.fly()
+    }
+
+    func crawl() -> String {
+        return lizard.crawl()
+    }
+}
